702358855d809cebe6e372f6e9a2b840c32b02e6
mirreinh <mirreinhh@gmail.com>
2021-02-07 14:25:07 +0300

    Install eslint-plugin-jest, fix linter error

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,6 +14,6 @@ const beforePath = getFixturePath('before.json');
 const afterPath = getFixturePath('after.json');
 
 test('genDiff', () => {
-    const result = readFile('result');
-    expect(genDiff(beforePath, afterPath)).toEqual(result);
-  });
+  const result = readFile('result');
+  expect(genDiff(beforePath, afterPath)).toEqual(result);
+});
