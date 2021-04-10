b9b4ed3813b74de6372477006e88bc036e9c21e7
mirreinh <mirreinhh@gmail.com>
2021-04-10 13:05:37 +0300

    Refactor AST building

--- src/renderDiff.js
+++ src/renderDiff.js
@@ -1,42 +1,33 @@
 import _ from 'lodash';
 
 const getAst = (before, after) => {
-  const beforeKeys = Object.keys(before);
-  const afterKeys = Object.keys(after);
-  const concatKeys = _.concat(beforeKeys, afterKeys);
-  const uniqKeys = _.uniq(concatKeys).sort();
+  const uniqKeys = _.union(Object.keys(before), Object.keys(after)).sort();
 
-  const state = [
+  const astHandlers = [
     {
       check: (key) => before[key] instanceof Object && after[key] instanceof Object,
       buildNode: (key) => ({ type: 'children', key, children: getAst(before[key], after[key]) }),
     },
     {
-      check: (key) => _.has(before, key)
-        && _.has(after, key) && (before[key] === after[key]),
+      check: (key) => _.has(before, key) && _.has(after, key) && (before[key] === after[key]),
       buildNode: (key) => ({ type: 'unchanged', key, valueAfter: after[key] }),
     },
     {
-      check: (key) => _.has(after, key)
-        && !_.has(before, key),
+      check: (key) => _.has(after, key) && !_.has(before, key),
       buildNode: (key) => ({ type: 'added', key, valueAfter: after[key] }),
     },
     {
-      check: (key) => _.has(before, key)
-        && !_.has(after, key),
+      check: (key) => _.has(before, key) && !_.has(after, key),
       buildNode: (key) => ({ type: 'deleted', key, valueBefore: before[key] }),
     },
     {
-      check: (key) => _.has(before, key)
-        && _.has(after, key) && (before[key] !== after[key]),
+      check: (key) => _.has(before, key) && _.has(after, key) && (before[key] !== after[key]),
       buildNode: (key) => ({
         type: 'changed', key, valueBefore: before[key], valueAfter: after[key],
       }),
     },
   ];
-  const ast = uniqKeys.map((key) => state.find(({ check }) => check(key))
-    .buildNode(key));
-  return ast;
+  return uniqKeys.map((key) => astHandlers.find(({ check }) => check(key)).buildNode(key));
 };
 
 export default getAst;
