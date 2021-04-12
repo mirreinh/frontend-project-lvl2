bd2ffe0b1d96ea4daa6594d38346c0f54bb36409
mirreinh <mirreinhh@gmail.com>
2021-04-12 17:44:18 +0300

    Create new formatter - json

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import tree from './stylish.js';
-import plane from './plane.js';
+import plane from './toPlane.js';
+import json from './toJson.js';
 
 const renderers = {
   tree,
   plane,
+  json,
 };
 
 export default (format) => renderers[format];
