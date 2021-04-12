ea3570ad17060fcf62bc6dd73c761a604487b09d
mirreinh <mirreinhh@gmail.com>
2021-04-12 17:35:51 +0300

    Configure commander, fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,31 +10,25 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const beforePath = getFixturePath('before.json');
+const afterPath = getFixturePath('after.json');
 
 test('genDiff json to tree', () => {
-  const beforePath = getFixturePath('before.json');
-  const afterPath = getFixturePath('after.json');
   const result = readFile('treeResult');
-  expect(genDiff(afterPath, beforePath, 'tree')).toEqual(result);
+  expect(genDiff(beforePath, afterPath, 'tree')).toEqual(result);
 });
 
 test('genDiff json to plane', () => {
-  const beforePath = getFixturePath('before.json');
-  const afterPath = getFixturePath('after.json');
   const result = readFile('planeResult');
-  expect(genDiff(afterPath, beforePath, 'plane')).toEqual(result);
+  expect(genDiff(beforePath, afterPath, 'plane')).toEqual(result);
 });
 
 test('genDiff yaml to tree', () => {
-  const beforePath = getFixturePath('before.yaml');
-  const afterPath = getFixturePath('after.yaml');
   const result = readFile('treeResult');
-  expect(genDiff(afterPath, beforePath, 'tree')).toEqual(result);
+  expect(genDiff(beforePath, afterPath, 'tree')).toEqual(result);
 });
 
 test('genDiff yaml to plane', () => {
-  const beforePath = getFixturePath('before.yaml');
-  const afterPath = getFixturePath('after.yaml');
   const result = readFile('planeResult');
-  expect(genDiff(afterPath, beforePath, 'plane')).toBe(result);
+  expect(genDiff(beforePath, afterPath, 'plane')).toBe(result);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 /* eslint-disable no-console, import/extensions */
-
+import commander from 'commander';
 import execute from '../index.js';
 
-const { program } = require('commander');
-
-program
+commander
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'tree')
+  .arguments('<filepath1> <filepath2>')
   .action((filePath1, filePath2, option) => {
     console.log(execute(filePath1, filePath2, option.format));
   });
-program.parse();
+commander.parse(process.argv);
