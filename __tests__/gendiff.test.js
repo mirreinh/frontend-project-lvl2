3af3b63e7269c8da9e5c45204d49b745737a6f63
mirreinh <mirreinhh@gmail.com>
2021-04-12 22:35:00 +0300

    Change tests and fix bugs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,25 +10,28 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const beforePath = getFixturePath('before.json');
-const afterPath = getFixturePath('after.json');
+const beforePathJson = getFixturePath('before.json');
+const afterPathJson = getFixturePath('after.json');
+
+const beforePathYaml = getFixturePath('before.yaml');
+const afterPathYaml = getFixturePath('after.yaml');
 
 test('genDiff json to tree', () => {
   const result = readFile('treeResult');
-  expect(genDiff(beforePath, afterPath, 'tree')).toEqual(result);
+  expect(genDiff(beforePathJson, afterPathJson, 'tree')).toEqual(result);
 });
 
 test('genDiff json to plane', () => {
   const result = readFile('planeResult');
-  expect(genDiff(beforePath, afterPath, 'plane')).toEqual(result);
+  expect(genDiff(beforePathJson, afterPathJson, 'plane')).toEqual(result);
 });
 
 test('genDiff yaml to tree', () => {
   const result = readFile('treeResult');
-  expect(genDiff(beforePath, afterPath, 'tree')).toEqual(result);
+  expect(genDiff(beforePathYaml, afterPathYaml, 'tree')).toEqual(result);
 });
 
 test('genDiff yaml to plane', () => {
   const result = readFile('planeResult');
-  expect(genDiff(beforePath, afterPath, 'plane')).toBe(result);
+  expect(genDiff(beforePathYaml, afterPathYaml, 'plane')).toBe(result);
 });
